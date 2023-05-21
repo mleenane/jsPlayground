d8cdcd2df6510a9ddc4a58e5ebf7cd833ac2e511
Martin Leenane <mleenane@icloud.com>
2023-05-21 20:57:30 +0100

    homeCode

--- js/script.js
+++ js/script.js
@@ -139,6 +139,28 @@ let describeCountry = function (country, population, capitalCity){
 };
 
 const descIreland = describeCountry("Ireland", 6, "Dublin");
-const descEngland = describeCountry("ENgland", 36, "London");
+const descEngland = describeCountry("England", 36, "London");
 console.log(descIreland);
 console.log(descEngland);
+console.log(describeCountry("Italy", 45, "Rome"));
+
+// Function Declaration
+function percentageOfWorld1(population){
+    return (population / 7900) * 100;
+};
+
+//Function Expression
+const percentageOfWorld2 = function (population) { 
+    return (population / 7900) * 100;
+};
+
+const percPortugal1 = percentageOfWorld1(10); 
+const percChina1 = percentageOfWorld1(1441); 
+const percUSA1 = percentageOfWorld1(332); 
+console.log(percPortugal1, percChina1, percUSA1);
+
+const percentageOfWorld3 = population => (population / 7900) * 100;
+const percPortugal3 = percentageOfWorld3(10); 
+const percChina3 = percentageOfWorld3(1441); 
+const percUSA3 = percentageOfWorld3(332); 
+console.log(percPortugal3, percChina3, percUSA3);
